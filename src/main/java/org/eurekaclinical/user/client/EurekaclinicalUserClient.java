dc9ca526b8e8ac1ae22de4f57e1233926e193f9b
Miao Ai <miaoai2010@gmail.com>
2016-09-09 16:19:13 -0400

    Add external use client EurekaclinicalUserClient to user-client project

--- src/main/java/org/eurekaclinical/user/client/EurekaclinicalUserClient.java
+++ src/main/java/org/eurekaclinical/user/client/EurekaclinicalUserClient.java
@@ -19,20 +19,109 @@
  */
 package org.eurekaclinical.user.client;
 
+import com.sun.jersey.api.client.GenericType;
+import java.util.List;
+import javax.ws.rs.core.UriBuilder;
+import org.eurekaclinical.common.comm.Role;
+import org.eurekaclinical.common.comm.clients.ClientException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.eurekaclinical.common.comm.clients.EurekaClinicalClient;
+import org.eurekaclinical.user.client.comm.OAuthProvider;
+import org.eurekaclinical.user.client.comm.PasswordChangeRequest;
+import org.eurekaclinical.user.client.comm.User;
+import org.eurekaclinical.user.client.comm.UserRequest;
 /**
- *
+ * EurekaclinicalUserClient is a client that should be used be external applications
+ * 
  * @author miaoai
  */
-public abstract class EurekaclinicalUserClient extends EurekaClinicalClient {
+public class EurekaclinicalUserClient extends EurekaClinicalClient {
 
 	private static final Logger LOGGER
 			= LoggerFactory.getLogger(EurekaclinicalUserClient.class);
         
-	protected EurekaclinicalUserClient() {
+	private static final GenericType<List<User>> UserList = new GenericType<List<User>>() {
+	};
+	private static final GenericType<List<Role>> RoleList = new GenericType<List<Role>>() {
+	};   
+       
+	private final String userServiceUrl;
+
+	public EurekaclinicalUserClient(String inUserServiceUrl) {
 		super(null);
-	}        
+		LOGGER.debug("Using eurekaclinical user service URL {}", inUserServiceUrl);
+		this.userServiceUrl = inUserServiceUrl;
+	}
+
+	@Override
+	protected String getResourceUrl() {
+		return this.userServiceUrl;
+	}
+
+	public List<User> getUsers() throws ClientException {
+		final String path = "/proxy-resource/users";
+		return doGet(path, UserList);
+	}
+        
+	public User getMe() throws ClientException {
+		String path = "/proxy-resource/users/me";
+		return doGet(path, User.class);
+	} 
+
+	public User getUserById(Long inUserId) throws ClientException {
+		final String path = "/proxy-resource/users/" + inUserId;
+		return doGet(path, User.class);
+	}
+
+	public void changePassword(String inOldPass, String inNewPass) throws ClientException {
+		final String path = "/proxy-resource/users/passwordchange";
+		PasswordChangeRequest passwordChangeRequest
+				= new PasswordChangeRequest();
+		passwordChangeRequest.setOldPassword(inOldPass);
+		passwordChangeRequest.setNewPassword(inNewPass);
+		doPost(path, passwordChangeRequest);
+	}
+
+	public void updateUser(User inUser, Long userId) throws ClientException {
+		final String path = "/proxy-resource/users/" + userId;
+		doPut(path, inUser);
+	}
+
+	public void addUser(UserRequest inRequest) throws ClientException {
+		final String path = "/api/userrequests";
+		doPostCreate(path, inRequest);
+	}     
+        
+	public void verifyUser(String inCode) throws ClientException {
+		final String path = "/api/userrequests/verify/" + inCode;
+		doPut(path);
+	} 
+        
+	public List<Role> getRoles() throws ClientException {
+		final String path = "/proxy-resource/roles";
+		return doGet(path, RoleList);
+	}    
+        
+	public Role getRole(Long inRoleId) throws ClientException {
+		final String path = "/proxy-resource/roles/" + inRoleId;
+		return doGet(path, Role.class);
+	}
+	
+	public Role getRoleByName(String name) throws ClientException {
+		return doGet("/proxy-resource/roles/byname/" + name, Role.class);
+	}
+
+	public OAuthProvider getOAuthProvider(Long inId) throws ClientException {
+		final String path = "/proxy-resource/oauthproviders/" + inId;
+		return doGet(path, OAuthProvider.class);
+	}
+
+	public OAuthProvider getOAuthProviderByName(String inName) throws ClientException {
+		final String path = UriBuilder.fromPath("/proxy-resource/oauthproviders/byname/")
+				.segment(inName)
+				.build().toString();
+		return doGet(path, OAuthProvider.class);
+	}           
 }
