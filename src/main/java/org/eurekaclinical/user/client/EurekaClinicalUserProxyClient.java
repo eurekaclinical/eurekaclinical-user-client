9078b1b388fc53b39ca152fd4e430a5fa031dd9f
Andrew Post <arpost@emory.edu>
2017-01-26 17:04:35 -0500

    Use the new authorizing client abstract classes.

--- src/main/java/org/eurekaclinical/user/client/EurekaClinicalUserProxyClient.java
+++ src/main/java/org/eurekaclinical/user/client/EurekaClinicalUserProxyClient.java
@@ -23,105 +23,91 @@ import com.sun.jersey.api.client.GenericType;
 import java.util.List;
 import javax.ws.rs.core.UriBuilder;
 import org.eurekaclinical.common.comm.Role;
+import org.eurekaclinical.common.comm.clients.AuthorizingEurekaClinicalProxyClient;
 import org.eurekaclinical.common.comm.clients.ClientException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.eurekaclinical.common.comm.clients.EurekaClinicalClient;
 import org.eurekaclinical.user.client.comm.OAuthProvider;
 import org.eurekaclinical.user.client.comm.PasswordChangeRequest;
 import org.eurekaclinical.user.client.comm.User;
 import org.eurekaclinical.user.client.comm.UserRequest;
+
 /**
- * EurekaclinicalUserClient is a client that should be used be external applications
- * 
+ * EurekaclinicalUserClient is a client that should be used be external
+ * applications
+ *
  * @author miaoai
  */
-public class EurekaClinicalUserProxyClient extends EurekaClinicalClient {
+public class EurekaClinicalUserProxyClient extends AuthorizingEurekaClinicalProxyClient {
 
-	private static final Logger LOGGER
-			= LoggerFactory.getLogger(EurekaClinicalUserProxyClient.class);
-        
-	private static final GenericType<List<User>> UserList = new GenericType<List<User>>() {
-	};
-	private static final GenericType<List<Role>> RoleList = new GenericType<List<Role>>() {
-	};   
-       
-	private final String userServiceUrl;
+    private static final Logger LOGGER
+            = LoggerFactory.getLogger(EurekaClinicalUserProxyClient.class);
 
-	public EurekaClinicalUserProxyClient(String inUserServiceUrl) {
-		super(null);
-		LOGGER.debug("Using eurekaclinical user service URL {}", inUserServiceUrl);
-		this.userServiceUrl = inUserServiceUrl;
-	}
+    private static final GenericType<List<User>> UserList = new GenericType<List<User>>() {
+    };
 
-	@Override
-	protected String getResourceUrl() {
-		return this.userServiceUrl;
-	}
+    private final String userServiceUrl;
 
-	public List<User> getUsers() throws ClientException {
-		final String path = "/api/protected/users";
-		return doGet(path, UserList);
-	}
-        
-	public User getMe() throws ClientException {
-		String path = "/api/protected/users/me";
-		return doGet(path, User.class);
-	} 
+    public EurekaClinicalUserProxyClient(String inUserServiceUrl) {
+        super(null);
+        LOGGER.debug("Using eurekaclinical user service URL {}", inUserServiceUrl);
+        this.userServiceUrl = inUserServiceUrl;
+    }
 
-	public User getUserById(Long inUserId) throws ClientException {
-		final String path = "/api/protected/users/" + inUserId;
-		return doGet(path, User.class);
-	}
+    @Override
+    protected String getResourceUrl() {
+        return this.userServiceUrl;
+    }
 
-	public void changePassword(String inOldPass, String inNewPass) throws ClientException {
-		final String path = "/api/protected/users/passwordchange";
-		PasswordChangeRequest passwordChangeRequest
-				= new PasswordChangeRequest();
-		passwordChangeRequest.setOldPassword(inOldPass);
-		passwordChangeRequest.setNewPassword(inNewPass);
-		doPost(path, passwordChangeRequest);
-	}
+    public List<User> getUsers() throws ClientException {
+        final String path = "/api/protected/users";
+        return doGet(path, UserList);
+    }
 
-	public void updateUser(User inUser, Long userId) throws ClientException {
-		final String path = "/api/protected/users/" + userId;
-		doPut(path, inUser);
-	}
+    public User getMe() throws ClientException {
+        String path = "/api/protected/users/me";
+        return doGet(path, User.class);
+    }
 
-	public void addUser(UserRequest inRequest) throws ClientException {
-		final String path = "/api/userrequests";
-		doPostCreate(path, inRequest);
-	}     
-        
-	public void verifyUser(String inCode) throws ClientException {
-		final String path = "/api/userrequests/verify/" + inCode;
-		doPut(path);
-	} 
-        
-	public List<Role> getRoles() throws ClientException {
-		final String path = "/api/protected/roles";
-		return doGet(path, RoleList);
-	}    
-        
-	public Role getRole(Long inRoleId) throws ClientException {
-		final String path = "/api/protected/roles/" + inRoleId;
-		return doGet(path, Role.class);
-	}
-	
-	public Role getRoleByName(String name) throws ClientException {
-		return doGet("/api/protected/roles/byname/" + name, Role.class);
-	}
+    public User getUserById(Long inUserId) throws ClientException {
+        final String path = "/api/protected/users/" + inUserId;
+        return doGet(path, User.class);
+    }
 
-	public OAuthProvider getOAuthProvider(Long inId) throws ClientException {
-		final String path = "/api/protected/oauthproviders/" + inId;
-		return doGet(path, OAuthProvider.class);
-	}
+    public void changePassword(String inOldPass, String inNewPass) throws ClientException {
+        final String path = "/api/protected/users/passwordchange";
+        PasswordChangeRequest passwordChangeRequest
+                = new PasswordChangeRequest();
+        passwordChangeRequest.setOldPassword(inOldPass);
+        passwordChangeRequest.setNewPassword(inNewPass);
+        doPost(path, passwordChangeRequest);
+    }
 
-	public OAuthProvider getOAuthProviderByName(String inName) throws ClientException {
-		final String path = UriBuilder.fromPath("/api/protected/oauthproviders/byname/")
-				.segment(inName)
-				.build().toString();
-		return doGet(path, OAuthProvider.class);
-	}           
+    public void updateUser(User inUser, Long userId) throws ClientException {
+        final String path = "/api/protected/users/" + userId;
+        doPut(path, inUser);
+    }
+
+    public void addUser(UserRequest inRequest) throws ClientException {
+        final String path = "/api/userrequests";
+        doPostCreate(path, inRequest);
+    }
+
+    public void verifyUser(String inCode) throws ClientException {
+        final String path = "/api/userrequests/verify/" + inCode;
+        doPut(path);
+    }
+
+    public OAuthProvider getOAuthProvider(Long inId) throws ClientException {
+        final String path = "/api/protected/oauthproviders/" + inId;
+        return doGet(path, OAuthProvider.class);
+    }
+
+    public OAuthProvider getOAuthProviderByName(String inName) throws ClientException {
+        final String path = UriBuilder.fromPath("/api/protected/oauthproviders/byname/")
+                .segment(inName)
+                .build().toString();
+        return doGet(path, OAuthProvider.class);
+    }
 }
